ecb98621cfdf2ae29f26f46e80566ac9e5ef3d38
adamkrejci <adamkrejci94@gmail.com>
2025-04-07 22:26:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -16,14 +16,19 @@ menuIcon.addEventListener("click", () => {
 });
 // Password comparison
 const form = document.getElementById("form");
-const emailInput_1 = document.querySelector(".password_1");
-const emailInput_2 = document.querySelector(".password_2");
+const password_Input_1 = document.querySelector(".password_1");
+const password_Input_2 = document.querySelector(".password_2");
 const resultText = document.querySelector(".text");
 
-emailInput_1.addEventListener("input", () => {
-  const emailValue_1 = emailInput_1.value;
-  const emailValue_2 = emailInput_2.value;
-  if (emailValue_2.match(emailValue_1)) {
+const validatePaswords = () => {
+  const password_Value_1 = password_Input_1.value;
+  const password_Value_2 = password_Input_2.value;
+
+  if (password_Value_1 === "") {
+    form.classList.remove("invalid");
+    form.classList.remove("valid");
+    resultText.textContent = "Heslo není vyplněné";
+  } else if (password_Value_2 === password_Value_1) {
     form.classList.add("valid");
     form.classList.remove("invalid");
     resultText.textContent = "Heslo je shodné.";
@@ -34,82 +39,20 @@ emailInput_1.addEventListener("input", () => {
     resultText.textContent = "Heslo není shodné.";
     resultText.style.color = "#ff0000";
   }
-  if (emailValue_2 === "") {
-    form.classList.remove("invalid");
-    form.classList.remove("valid");
-    resultText.textContent = "Heslo není vyplněné";
-  }
-});
+};
+password_Input_1.addEventListener("input", validatePaswords);
+password_Input_2.addEventListener("input", validatePaswords);
 
-emailInput_2.addEventListener("input", () => {
-  const emailValue_1 = emailInput_1.value;
-  const emailValue_2 = emailInput_2.value;
-  if (emailValue_2.match(emailValue_1)) {
-    form.classList.add("valid");
-    form.classList.remove("invalid");
-    resultText.textContent = "Heslo je shodné.";
-    resultText.style.color = "#00ff00";
-  } else {
-    form.classList.add("invalid");
-    form.classList.remove("valid");
-    resultText.textContent = "Heslo není shodné.";
-    resultText.style.color = "#ff0000";
-  }
-  if (emailValue_2 === "") {
-    form.classList.remove("invalid");
-    form.classList.remove("valid");
-    resultText.textContent = "Heslo není vyplněné";
-  }
-});
-
-// DARK MODE
-// const body = document.querySelector("body");
-// const header = document.querySelector("header");
-// const links = document.querySelectorAll("nav ul li a");
-// const menu_icon = document.querySelector(".menu-icon");
-// const links_mobile = document.querySelectorAll("header nav");
-
-// btnLight.addEventListener("click", () => {
-//   body.style.backgroundColor = "white";
-//   body.style.color = "black";
-//   header.style.backgroundColor = "#4d7282";
-//   links.forEach((links) => {
-//     links.style.color = "black";
-//   });
-//   menu_icon.style.color = "black";
-//   links_mobile.forEach((links_mobile) => {
-//     links_mobile.style.backgroundColor = "#4d7282";
-//   });
-// });
-// btnDark.addEventListener("click", () => {
-//   body.style.backgroundColor = "black";
-//   body.style.color = "white";
-//   header.style.backgroundColor = "#15304c";
-//   links.forEach((links) => {
-//     links.style.color = "white";
-//   });
-//   menu_icon.style.color = "white";
-//   links_mobile.forEach((links_mobile) => {
-//     links_mobile.style.backgroundColor = "#15304c";
-//   });
-// });
-//body
-
-//   body.style.backgroundColor = "white";
-//   body.style.color = "black";
-//   header.style.backgroundColor = "#4d7282";
-//   links.forEach((links) => {
-//     links.style.color = "black";
-//   })
-const body = document.querySelector("body"); // Get the DIV element
+//light - dark mode
+const body = document.querySelector("body");
 const header = document.querySelector("header");
 const nav_li_a = document.querySelectorAll("li a");
 const header_nav = document.querySelectorAll("header nav");
 function toDark() {
-  body.classList.remove("body-light"); // Remove mystyle class from DIV
-  body.classList.add("body-dark"); // Add newone class to DIV
-  header.classList.remove("header-light"); // Remove mystyle class from DIV
-  header.classList.add("header-dark"); // Add newone class to DIV
+  body.classList.remove("body-light");
+  body.classList.add("body-dark");
+  header.classList.remove("header-light");
+  header.classList.add("header-dark");
   nav_li_a.forEach((link) => {
     link.classList.remove("link-light");
     link.classList.add("link-dark");
@@ -119,12 +62,11 @@ function toDark() {
     link.classList.add("header_nav-dark");
   });
 }
-
 function toLight() {
-  body.classList.remove("body-dark"); // Remove mystyle class from DIV
-  body.classList.add("body-light"); // Add newone class to DIV
-  header.classList.remove("header-dark"); // Remove mystyle class from DIV
-  header.classList.add("header-light"); // Add newone class to DIV
+  body.classList.remove("body-dark");
+  body.classList.add("body-light");
+  header.classList.remove("header-dark");
+  header.classList.add("header-light");
   nav_li_a.forEach((link) => {
     link.classList.remove("link-dark");
     link.classList.add("link-light");
@@ -136,53 +78,18 @@ function toLight() {
 }
 document.getElementById("btdark").addEventListener("click", toDark);
 document.getElementById("btlight").addEventListener("click", toLight);
-// //header
-// //links
-// const links = document.querySelectorAll("nav ul li a");
-// function toDark() {
-//   element.body.remove("links-light"); // Remove mystyle class from DIV
-//   element.body.add("links-dark"); // Add newone class to DIV
-// }
 
-// function toLight() {
-//   element.body.remove("links-dark"); // Remove mystyle class from DIV
-//   element.body.add("links-light"); // Add newone class to DIV
-// }
-// //header nav
-// const links = document.querySelectorAll("header nav");
-// function toDark() {
-//   element.body.remove("nav-light"); // Remove mystyle class from DIV
-//   element.body.add("nav-dark"); // Add newone class to DIV
-// }
-
-// function toLight() {
-//   element.body.remove("nav-dark"); // Remove mystyle class from DIV
-//   element.body.add("nav-light"); // Add newone class to DIV
-// }
-// //menu icon
-// const menu_icon = document.querySelector(".menu-icon");
-// function toDark() {
-//   element.body.remove("menu_icon-light"); // Remove mystyle class from DIV
-//   element.body.add("menu_icon-dark"); // Add newone class to DIV
-// }
-
-// function toLight() {
-//   element.body.remove("menu_icon-dark"); // Remove mystyle class from DIV
-//   element.body.add("menu_icon-light"); // Add newone class to DIV
-// }
-
+//rolling to start button
 const mybutton = document.getElementById("scrollToTopBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+window.addEventListener("scroll", function () {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
-};
+});
 
-// Add an event listener to the button for the "click" event
 mybutton.addEventListener("click", function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
